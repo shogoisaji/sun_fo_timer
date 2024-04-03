4441e2b9076cac764ec49098adc43adfda944e74
shogoisaji <simacsimac9@gmail.com>
2024-04-03 11:04:43 +0900

    ✨  Add license

---
+++ lib/util/license_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'license_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$licenseNotifierHash() => r'a64299c2c0085d51534dbe850fd0f42ad5b9a97a';
+
+/// See also [LicenseNotifier].
+@ProviderFor(LicenseNotifier)
+final licenseNotifierProvider = AutoDisposeAsyncNotifierProvider<
+    LicenseNotifier, List<LicenseEntry>>.internal(
+  LicenseNotifier.new,
+  name: r'licenseNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$licenseNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LicenseNotifier = AutoDisposeAsyncNotifier<List<LicenseEntry>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
